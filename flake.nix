774b012871b47d820242c65bd3478ef01d58053a
Adam JOLY <adjoly@student.42angouleme.fr>
2025-01-04 17:58:17 +0100

    「✨」 feat: fuck bash

--- flake.nix
+++ flake.nix
@@ -1,9 +1,33 @@
 {
 	inputs = {
-
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		pogit = {
+			url = "github:y-syo/pogit";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
-	outputs = {
+	outputs = inputs@{ nixpkgs, ... }: 
+		let 
+			supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+			forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+				pkgs = import nixpkgs { inherit system; };
+			});
+		in {
+			devShells = forEachSupportedSystem ({ pkgs }: {
+				default = pkgs.mkShell.override
+				{}
+				{
+					buildInputs = with pkgs;[
 
-	};
+					];
+					packages = with pkgs; [
+						git
+						vault
+						inputs.pogit.packages.${pkgs.system}.default
+					];
+				};
+			});
+		};
 }
+
