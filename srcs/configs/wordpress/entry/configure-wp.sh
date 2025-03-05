71f5624b1db4c3d5cddcb2bbaba3e78fead61531
adjoly <adamjly@proton.me>
2025-03-05 17:57:14 +0100

    「🏗️」 wip: Should be working

--- srcs/configs/wordpress/entry/configure-wp.sh
+++ srcs/configs/wordpress/entry/configure-wp.sh
@@ -16,8 +16,7 @@ if [ -f "${WP_DIR}/wp-config.php" ]; then
 else
 	echo "Confiruring wordpress..."
 	sleep 1
-	echo $TZ
-	wp --allow-root core config --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS} --dbhost=${WP_DB_HOST} --dbprefix=wp_ --path=${WP_DIR}
-	wp --allow-root core install --url=https://${WP_URL} --title="${WP_TITLE}" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --path=${WP_DIR}
+	wp --allow-root core config --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=$(getFileEnv WP_DB_PASS) --dbhost=${WP_DB_HOST} --dbprefix=wp_ --path=${WP_DIR}
+	wp --allow-root core install --url=https://${WP_URL} --title="${WP_TITLE}" --admin_user=${WP_ADMIN_USER} --admin_password=$(getFileEnv WP_ADMIN_PASS) --admin_email=${WP_ADMIN_EMAIL} --path=${WP_DIR}
 	wp option update blog_public ${WP_SEARCH_ENGINE_VISIBILITY} --allow-root
 fi
